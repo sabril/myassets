1cfbb348fe4d00a218c079c9a30ebaa3367afd57
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2014-08-11 20:05:42 +0800

    load prices faster

---
+++ config/unicorn.rb
@@ -0,0 +1,25 @@
+worker_processes 3
+timeout 180
+preload_app true
+
+before_fork do |server, worker|
+  #@sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  #@delayed_job_pid ||= spawn("bundle exec rake jobs:work")
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
