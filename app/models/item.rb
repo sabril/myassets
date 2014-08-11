a898dbdace21bcb137e62d38b57b0655b918dbc7
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2014-08-11 19:12:09 +0800

    value collections

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item
   field :name
   field :buy_value, type: Integer
   field :buy_date
-  field :quantity, type: Integer
+  field :quantity, type: Integer, default: 10
   belongs_to :user
   validates_presence_of :name
   def unit_value
@@ -16,5 +16,9 @@ class Item
   
   def current_value
     quantity * GoldPrice.get_logam_mulia_price[0].to_i
+  end
+  
+  def self.quantity_list
+    {"1 gr" => 1, "2 gr" => 2, "3 gr" => 3, "5 gr" => 5, "10 gr" => 10, "25 gr" => 25, "50 gr" => 50, "100 gr" => 100}
   end
 end
